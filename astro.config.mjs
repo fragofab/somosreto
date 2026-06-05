d9cafe16586e225890329483594c58a6b9827727
Fabian Delgado <fabian.fragoso@me.com>
2026-06-04 23:59:30 -0600

    fix(assets): use BASE_URL for all local asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
   site: 'https://fragofab.github.io',
-  base: isProd ? '/reto-website' : '/',
+  base: '/reto-website',
   output: 'static',
 });
