a89f4ad9d4da95be9ded1cae7e52f35d70305917
Fabian Delgado <fabian.fragoso@me.com>
2026-06-05 17:30:08 -0600

    chore: rename project from reto-website to somosreto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   site: 'https://fragofab.github.io',
-  base: '/reto-website',
+  base: isProd ? '/somosreto' : '/',
   output: 'static',
 });
